3582e614dca2620da925faa69884270a13c02bde
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-07 14:42:31 +0600

    showing Country with Capital using for loop

--- countryScript.js
+++ countryScript.js
@@ -1,17 +1,32 @@
 fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
-    .then( data => {
-        data.forEach( country=> {
-            const countryDetailsDiv = document.getElementById('countryDetails');
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            const country = data[i]
+            const countryContainer = document.getElementById('countryDetails');
             const countryDiv = document.createElement('div')
             countryDiv.className = "country"
-            const countryName = document.createElement('h3');
-            const capitalName = document.createElement('p');
-            countryName.innerText = country.name;
-            capitalName.innerText = country.capital;
-            countryDiv.appendChild(countryName)
-            countryDiv.appendChild(capitalName)
-            countryDetailsDiv.appendChild(countryDiv)
-        })
-        
-    })
+            const countryDetails =
+                `<h3 class="country-name">${country.name}</h3>
+                <p class="capital-name">${country.capital}</p>
+                `
+            countryDiv.innerHTML = countryDetails;
+            countryContainer.appendChild(countryDiv)
+        }
+
+    });
+
+
+
+// data.forEach( country=> {
+        //     const countryDetailsDiv = document.getElementById('countryDetails');
+        //     const countryDiv = document.createElement('div')
+        //     countryDiv.className = "country"
+        //     const countryName = document.createElement('h3');
+        //     const capitalName = document.createElement('p');
+        //     countryName.innerText = country.name;
+        //     capitalName.innerText = country.capital;
+        //     countryDiv.appendChild(countryName)
+        //     countryDiv.appendChild(capitalName)
+        //     countryDetailsDiv.appendChild(countryDiv)
+        // })

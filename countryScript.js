f39f88cc92488651ceea111ea14ed2087d4100f0
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-07 16:17:40 +0600

    Show More Details

--- countryScript.js
+++ countryScript.js
@@ -3,12 +3,13 @@ fetch('https://restcountries.eu/rest/v2/all')
     .then(data => {
         for (let i = 0; i < data.length; i++) {
             const country = data[i]
-            const countryContainer = document.getElementById('countryDetails');
+            const countryContainer = document.getElementById('ShowCountry');
             const countryDiv = document.createElement('div')
             countryDiv.className = "country"
             const countryDetails =
                 `<h3 class="country-name">${country.name}</h3>
                 <p class="capital-name">${country.capital}</p>
+                <button onclick="showCountryDetails('${country.name}')">Details</button>
                 `
             countryDiv.innerHTML = countryDetails;
             countryContainer.appendChild(countryDiv)
@@ -16,17 +17,21 @@ fetch('https://restcountries.eu/rest/v2/all')
 
     });
 
+const showCountryDetails = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            renderCountryInfo(data[0]);
+        })
+}
 
-
-// data.forEach( country=> {
-        //     const countryDetailsDiv = document.getElementById('countryDetails');
-        //     const countryDiv = document.createElement('div')
-        //     countryDiv.className = "country"
-        //     const countryName = document.createElement('h3');
-        //     const capitalName = document.createElement('p');
-        //     countryName.innerText = country.name;
-        //     capitalName.innerText = country.capital;
-        //     countryDiv.appendChild(countryName)
-        //     countryDiv.appendChild(capitalName)
-        //     countryDetailsDiv.appendChild(countryDiv)
-        // })
+const renderCountryInfo = country => {
+    const countryDetails = document.getElementById('countryDetails');
+    countryDetails.innerHTML = `
+        <h2>Name : ${country.name}</h2>
+        <p>Capital : ${country.capital}</p>
+        <p>Numeric Code : ${country.numericCode}</p>
+        <img src="${country.flag}" alt="">
+        `
+}
